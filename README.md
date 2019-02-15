ef5215629a8c2a60bde67e07ffc64c2b52042ea2
Thomas Patry <thompatry@gmail.com>
2019-02-14 23:03:12 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Zabbix PiHole template
+# Tested on Zabbix 4.0.3
+
+## Installation (Generic x86_64)
+- Install jq: `sudo apt install jq`
+- Copy `pihole_data.sh` to `/etc/zabbix/bin`: `sudo cp pihole_data.sh /etc/zabbix/bin` (If the bin directory does not exist, create it `sudo mkdir /etc/zabbix/bin`)
+- Make it executable: `sudo chmod a+x /etc/zabbix/bin/pihole_data.sh`
+- Install the systemd service and timer: `sudo cp pihole_data.service /etc/systemd/system` and `sudo cp pihole_data.timer /etc/systemd/system`
+- Start and enable the timer: `systemctl enable --now pihole_data.timer`
+- Copy pihole_data.conf into /etc/zabbix/zabbix-agentd.d: `sudo cp pihole_data.conf /etc/zabbix/zabbix_agentd.d`
+- Restart zabbix-agent: `sudo systemctl restart zabbix-agent`
+- Import `template_pihole_data.xml` on your Zabbix server
+- Add template to server that has the pihole_data.sh service on in Zabbix
---
+++ pihole_data.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+set -e
+
+pihole_server=localhost
+
+CACHE_FILE=/var/log/zabbix/pihole-data.log
+LOCK_FILE=/tmp/pihole-data.lock
+
+run_pihole_query() {
+    cd "$(readlink -f "$(dirname "$0")")" || exit 9
+
+    # Lock
+    if [[ -e "$LOCK_FILE" ]]
+    then
+        echo "Already getting data from pihole" >&2
+        exit 2
+    fi
+    touch "$LOCK_FILE"
+    trap "rm -rf $LOCK_FILE" EXIT HUP INT QUIT PIPE TERM
+
+    local domains_blocked dns_queries_today ads_blocked_today ads_percentage_today
+
+    get_domainsblocked(){
+        rawjson=$(curl -s http://$pihole_server/admin/api.php | jq '.domains_being_blocked')
+        domains_blocked=$(echo $rawjson | sed -e 's/\"//g' -e 's/\,//g')
+    }
+    get_dnsqueriestoday(){
+        rawjson=$(curl -s http://$pihole_server/admin/api.php | jq '.dns_queries_today')
+        dns_queries_today=$(echo $rawjson | sed -e 's/\"//g' -e 's/\,//g')
+    }
+    get_adsblockedtoday(){
+        rawjson=$(curl -s http://$pihole_server/admin/api.php | jq '.ads_blocked_today')
+        ads_blocked_today=$(echo $rawjson | sed -e 's/\"//g' -e 's/\,//g')
+    }
+    get_adspercentagetoday(){
+        rawjson=$(curl -s http://$pihole_server/admin/api.php | jq '.ads_percentage_today')
+        ads_percentage_today=$(echo $rawjson | sed -e 's/\"//g' -e 's/\,//g')
+        }
+
+    get_domainsblocked
+    get_dnsqueriestoday
+    get_adsblockedtoday
+    get_adspercentagetoday
+
+    {
+        echo "Domains Blocked: $domains_blocked"
+        echo "DNS Queries Today: $dns_queries_today"
+        echo "Ads Blocked Today: $ads_blocked_today"
+        echo "Ads Percentage Today: $ads_percentage_today"
+    } > "$CACHE_FILE"
+
+    # Make sure to remove the lock file (may be redundant)
+    rm -rf "$LOCK_FILE"
+}
+
+case "$1" in
+    -c|--cached)
+        cat "$CACHE_FILE"
+        ;;
+    -do|--domains)
+        awk '/Domains Blocked/ { print }' "$CACHE_FILE"
+        ;;
+    -dn|--dns)
+        awk '/DNS Queries Today/ { print }' "$CACHE_FILE"
+        ;;
+    -ab|--ads-blocked)
+        awk '/Ads Blocked Today/ { print }' "$CACHE_FILE"
+        ;;
+    -ap|--ads-percentage)
+        awk '/Ads Percentage Today/ { print }' "$CACHE_FILE"
+        ;;
+    -f|--force)
+        rm -rf "$LOCK_FILE"
+        run_pihole_query
+        ;;
+    *)
+        run_pihole_query
+        ;;
+esac
